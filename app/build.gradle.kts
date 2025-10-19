a6dbcc5d843a68a2145fbceed82d35b974b6e786
Cesar Martinez <cesar_a_mar_@hotmail.com>
2025-10-19 11:32:27 -0500

    Se agrega encabezado vendedor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.lottie)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebaseDatabase)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/app/tiendavirtual/SplashScreenActivity.kt
+++ app/src/main/java/com/app/tiendavirtual/SplashScreenActivity.kt
@@ -3,13 +3,21 @@ package com.app.tiendavirtual
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.app.tiendavirtual.cliente.MainActivityCliente
+import com.app.tiendavirtual.vendedor.MainActivityVendedor
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         verBienvenida()
     }
@@ -24,5 +32,15 @@ class SplashScreenActivity : AppCompatActivity() {
                 finish()
             }
         }.start()
+    }
+
+    private fun comprobarTipoUsuario(){
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == null) {
+            //startActivity(Intent(this, RegistroClienteActivity::class.java))
+            Toast.makeText(applicationContext, "No se encuentra registrado", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Ha iniciado sesión", Toast.LENGTH_SHORT).show()
+        }
     }
 }
