992603aece07394088d94c222476e035634ec88e
Cesar Martinez <cesar_a_mar_@hotmail.com>
2025-10-19 00:26:51 -0500

    Enlazar BottomNavigationBar con ActivityMain

--- app/src/main/java/com/app/tiendavirtual/cliente/MainActivityCliente.kt
+++ app/src/main/java/com/app/tiendavirtual/cliente/MainActivityCliente.kt
@@ -1,12 +1,73 @@
 package com.app.tiendavirtual.cliente
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import com.app.tiendavirtual.R
+import com.app.tiendavirtual.cliente.bottom_nav_fragments_cliente.FragmentMisOrdenesC
+import com.app.tiendavirtual.cliente.bottom_nav_fragments_cliente.FragmentTiendaC
+import com.app.tiendavirtual.cliente.nav_fragments_cliente.FragmentInicioC
+import com.app.tiendavirtual.cliente.nav_fragments_cliente.FragmentMiPerfilC
+import com.app.tiendavirtual.databinding.ActivityMainClienteBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivityCliente : AppCompatActivity() {
+class MainActivityCliente : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityMainClienteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_cliente)
+        binding = ActivityMainClienteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
+        val toogle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            toolbar,
+            R.string.open_drawer,
+            R.string.close_drawner
+        )
+        binding.drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+        replaceFragment(FragmentInicioC())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.navFragment,fragment)
+            .commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.op_inicio_c->{
+                replaceFragment(FragmentInicioC())
+            }
+            R.id.op_mi_perfil_c->{
+                replaceFragment(FragmentMiPerfilC())
+            }
+            R.id.op_cerrar_sesion_c->{
+                Toast.makeText(applicationContext, "Has cerrado sesión", Toast.LENGTH_SHORT).show()
+            }
+            R.id.op_tienda_c->{
+                replaceFragment(FragmentTiendaC())
+            }
+            R.id.op_mis_ordenes_c->{
+                replaceFragment(FragmentMisOrdenesC())
+            }
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
--- app/src/main/java/com/app/tiendavirtual/cliente/bottom_nav_fragments_cliente/FragmentTiendaC.kt
+++ app/src/main/java/com/app/tiendavirtual/cliente/bottom_nav_fragments_cliente/FragmentTiendaC.kt
@@ -6,15 +6,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.app.tiendavirtual.R
+import com.app.tiendavirtual.databinding.FragmentInicioCBinding
 
 class FragmentTiendaC : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tienda_c, container, false)
+    private lateinit var binding : FragmentInicioCBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentInicioCBinding.inflate(inflater,container,false)
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.op_tienda_c->{
+                    replaceFragment(FragmentTiendaC())
+                }
+                R.id.op_mis_ordenes_c->{
+                    replaceFragment(FragmentMisOrdenesC())
+                }
+            }
+            true
+        }
+        replaceFragment(FragmentTiendaC())
+        binding.bottomNavigation.selectedItemId = R.id.op_tienda_c
+        return binding.root
     }
 
+    private fun replaceFragment(fragment: Fragment) {
+        parentFragmentManager
+            .beginTransaction()
+            .replace(R.id.bottomFragment,fragment)
+            .commit()
+    }
 }
