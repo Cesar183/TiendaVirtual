40c16af674b94589071bb8c6b3deb098a01c4285
Cesar Martinez <cesar_a_mar_@hotmail.com>
2025-10-21 21:38:07 -0500

    Se agrega pantalla registro de vendedor

---
+++ app/src/main/java/com/app/tiendavirtual/vendedor/RegistroVendedorActivity.kt
@@ -0,0 +1,12 @@
+package com.app.tiendavirtual.vendedor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.app.tiendavirtual.R
+
+class RegistroVendedorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_vendedor)
+    }
+}
