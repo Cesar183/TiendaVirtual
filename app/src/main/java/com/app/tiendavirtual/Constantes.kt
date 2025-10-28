1b34c5c9d92a0ceda401f5bffc80765ed3850c16
Cesar Martinez <cesar_a_mar_@hotmail.com>
2025-10-28 17:55:33 -0500

    Registro de vendedor

---
+++ app/src/main/java/com/app/tiendavirtual/Constantes.kt
@@ -0,0 +1,7 @@
+package com.app.tiendavirtual
+
+class Constantes {
+    fun obtenerTiempo() : Long {
+        return System.currentTimeMillis()
+    }
+}
--- app/src/main/java/com/app/tiendavirtual/vendedor/RegistroVendedorActivity.kt
+++ app/src/main/java/com/app/tiendavirtual/vendedor/RegistroVendedorActivity.kt
@@ -1,12 +1,113 @@
 package com.app.tiendavirtual.vendedor
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.app.tiendavirtual.R
+import com.app.tiendavirtual.Constantes
+import com.app.tiendavirtual.databinding.ActivityRegistroVendedorBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class RegistroVendedorActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityRegistroVendedorBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro_vendedor)
+        binding = ActivityRegistroVendedorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.btnRegistrarV.setOnClickListener{
+            validarInformacion()
+        }
+    }
+
+    private var nombres = ""
+    private var email = ""
+    private var password = ""
+    private var cpassword = ""
+    private fun validarInformacion(){
+        nombres = binding.etNombresV.text.toString().trim()
+        email = binding.etEmailV.text.toString().trim()
+        password = binding.etPasswordV.text.toString().trim()
+        cpassword = binding.etCPassword.text.toString().trim()
+
+        if (nombres.isEmpty()) {
+            binding.etNombresV.error = "Ingrese sus nombres"
+            binding.etNombresV.requestFocus()
+        } else if (email.isEmpty()){
+            binding.etEmailV.error = "Ingrese su correo"
+            binding.etEmailV.requestFocus()
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.etEmailV.error = "Email no válido"
+            binding.etEmailV.requestFocus()
+        } else if (password.isEmpty()){
+            binding.etPasswordV.error = "Ingrese password"
+            binding.etPasswordV.requestFocus()
+        } else if (password.length <= 6){
+            binding.etPasswordV.error = "Necesita 6 o mas caracteres"
+            binding.etPasswordV.requestFocus()
+        } else if (cpassword.isEmpty()){
+            binding.etCPassword.error = "Confirme password"
+            binding.etCPassword.requestFocus()
+        } else if (password != cpassword){
+            binding.etCPassword.error = "No coincide el password"
+            binding.etCPassword.requestFocus()
+        } else {
+            registrarVendedor()
+        }
+    }
+
+    private fun registrarVendedor() {
+        progressDialog.setMessage("Creando cuenta")
+        progressDialog.show()
+        
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener { 
+                insertarInfoBD()
+            }
+            .addOnFailureListener{ e ->
+                Toast.makeText(this, "Falló el registro debido a ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun insertarInfoBD() {
+        progressDialog.setMessage("Guardando información...")
+
+        val uidBD = firebaseAuth.uid
+        val nombreBD = nombres
+        val emailBD = email
+        val tiempoBD = Constantes().obtenerTiempo()
+        val datosVendedor = HashMap<String, Any>()
+
+        datosVendedor["uid"] = "$uidBD"
+        datosVendedor["nombres"] = "$nombreBD"
+        datosVendedor["email"] = "$emailBD"
+        datosVendedor["tipoUsuario"] = "vendedor"
+        datosVendedor["tiempoRegistro"] = tiempoBD
+
+        val references = FirebaseDatabase.getInstance().getReference("Usuarios")
+        references.child(uidBD!!)
+            .setValue(datosVendedor)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                startActivity(Intent(this, MainActivityVendedor::class.java))
+                finish()
+            }
+            .addOnFailureListener { e ->
+                progressDialog.dismiss()
+                Toast.makeText(this, "Falló el registro en BD debido a ${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
