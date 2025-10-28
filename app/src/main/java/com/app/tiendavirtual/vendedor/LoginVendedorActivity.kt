3587cc6e17eab7a95c443d0a3724c439d1b91406
Cesar Martinez <cesar_a_mar_@hotmail.com>
2025-10-28 18:58:43 -0500

    Inicio sesion vendedor

--- app/src/main/java/com/app/tiendavirtual/vendedor/LoginVendedorActivity.kt
+++ app/src/main/java/com/app/tiendavirtual/vendedor/LoginVendedorActivity.kt
@@ -1,12 +1,74 @@
 package com.app.tiendavirtual.vendedor
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.app.tiendavirtual.R
+import com.app.tiendavirtual.databinding.ActivityLoginVendedorBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginVendedorActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginVendedorBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_vendedor)
+        binding = ActivityLoginVendedorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.btnLoginV.setOnClickListener {
+            validarInfo()
+        }
+        binding.tvRegistrarV.setOnClickListener {
+            startActivity(Intent(applicationContext, RegistroVendedorActivity::class.java))
+        }
+    }
+
+    private var email = ""
+    private var password = ""
+    private fun validarInfo() {
+        email = binding.etEmailV.text.toString().trim()
+        password = binding.etPasswordV.text.toString().trim()
+
+        if (email.isEmpty()){
+            binding.etEmailV.error = "Ingrese email"
+            binding.etEmailV.requestFocus()
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.etEmailV.error = "Email no válido"
+            binding.etEmailV.requestFocus()
+        } else if (password.isEmpty()){
+            binding.etEmailV.error = "Ingrese password"
+            binding.etEmailV.requestFocus()
+        } else {
+            loginVendedor()
+        }
+    }
+
+    private fun loginVendedor() {
+        progressDialog.setMessage("Ingresando")
+        progressDialog.show()
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                startActivity(Intent(this, MainActivityVendedor::class.java))
+                finishAffinity()
+                Toast.makeText(this, "Bienvenido(a)", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                progressDialog.dismiss()
+                Toast.makeText(this, "No se pudo iniciar sesion ${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
