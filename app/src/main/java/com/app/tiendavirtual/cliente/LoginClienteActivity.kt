159396c1c3ac500c5cd149c96dadc02ab8d57a53
Cesar Martinez <cesar_a_mar_@hotmail.com>
2025-11-02 23:22:19 -0500

    Login cliente UI

---
+++ app/src/main/java/com/app/tiendavirtual/cliente/LoginClienteActivity.kt
@@ -0,0 +1,12 @@
+package com.app.tiendavirtual.cliente
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.app.tiendavirtual.R
+
+class LoginClienteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_cliente)
+    }
+}
