ab16056c913c0b6c4ce8731acf0d060c7762bf40
Cesar Martinez <cesar_a_mar_@hotmail.com>
2025-11-04 17:44:38 -0500

    Se crea logica registro cliente

--- app/src/main/java/com/app/tiendavirtual/cliente/LoginClienteActivity.kt
+++ app/src/main/java/com/app/tiendavirtual/cliente/LoginClienteActivity.kt
@@ -1,12 +1,22 @@
 package com.app.tiendavirtual.cliente
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.app.tiendavirtual.R
+import com.app.tiendavirtual.databinding.ActivityLoginClienteBinding
 
 class LoginClienteActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginClienteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_cliente)
+        binding = ActivityLoginClienteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvRegistrarC.setOnClickListener {
+            startActivity(Intent(this@LoginClienteActivity, RegistroClienteActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/app/tiendavirtual/cliente/RegistroClienteActivity.kt
+++ app/src/main/java/com/app/tiendavirtual/cliente/RegistroClienteActivity.kt
@@ -1,12 +1,112 @@
 package com.app.tiendavirtual.cliente
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.app.tiendavirtual.Constantes
 import com.app.tiendavirtual.R
+import com.app.tiendavirtual.databinding.ActivityRegistroClienteBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class RegistroClienteActivity : AppCompatActivity() {
+
+    private lateinit var binding :  ActivityRegistroClienteBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro_cliente)
+        binding = ActivityRegistroClienteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.btnRegistrarC.setOnClickListener {
+            validarInformacion()
+        }
+    }
+
+    private var nombres = ""
+    private var email = ""
+    private var password = ""
+    private var cpassword = ""
+    private fun validarInformacion() {
+        nombres = binding.etNombresC.text.toString().trim()
+        email = binding.etEmail.text.toString().trim()
+        password = binding.etPassword.text.toString().trim()
+        cpassword = binding.etCPassword.text.toString().trim()
+
+        if (nombres.isEmpty()){
+            binding.etNombresC.error = "Ingrese nombres"
+            binding.etNombresC.requestFocus()
+        } else if (email.isEmpty()){
+            binding.etEmail.error = "Ingrese email"
+            binding.etEmail.requestFocus()
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.etEmail.error = "Email inválido"
+            binding.etEmail.requestFocus()
+        } else if (password.isEmpty()) {
+            binding.etPassword.error = "Ingrese password"
+            binding.etPassword.requestFocus()
+        } else if (password.length < 6) {
+            binding.etPassword.error = "Necesita mas de 6 caracteres"
+            binding.etPassword.requestFocus()
+        } else if (password != cpassword){
+            binding.etCPassword.error = "No coinciden los password"
+            binding.etCPassword.requestFocus()
+        } else {
+            registrarCliente()
+        }
+    }
+
+    private fun registrarCliente() {
+        progressDialog.setMessage("Creando cuenta")
+        progressDialog.show()
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                insertarInfoBD()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "Falló el registro debido a ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun insertarInfoBD() {
+        progressDialog.setMessage("Guardando información")
+
+        val uid = firebaseAuth.uid
+        val nombresC = nombres
+        val email = email
+        val tiempoRegistro = Constantes().obtenerTiempo()
+        val datosCliente = HashMap<String, Any>()
+
+        datosCliente["uid"] = "$uid"
+        datosCliente["nombresC"] = "$nombresC"
+        datosCliente["email"] = "$email"
+        datosCliente["tRegistro"] = "$tiempoRegistro"
+        datosCliente["imagen"] = ""
+        datosCliente["tipoUsuario"] = "cliente"
+
+        val reference = FirebaseDatabase.getInstance().getReference("Usuarios")
+        reference.child(uid!!)
+            .setValue(datosCliente)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                startActivity(Intent(this@RegistroClienteActivity, MainActivityCliente::class.java))
+                finishAffinity()
+            }
+            .addOnFailureListener {e ->
+                progressDialog.dismiss()
+                Toast.makeText(this, "Falló el registro debido a ${e.message}", Toast.LENGTH_LONG).show()
+            }
     }
 }
